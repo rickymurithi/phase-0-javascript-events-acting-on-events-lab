c6bc5ae8297aae9c6d8e462785274f10cf2d5560
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-09-05 22:48:36 +0300

    add file

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+//code!
+document.addEventListener('keyup', 
+  function(e) {
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    } else if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+  })
+
+  function moveDodgerLeft() {
+    let leftNum = dodgerLeft.style.left.replace('px', '');
+    let left = parseInt(leftNum, 10);
+
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+
+  }
+
+  function moveDodgerRight() {
+    let leftNum = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNum, 10);
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
